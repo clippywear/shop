bfb532bbdecfc69219afa0dc593911aea1e38c4b
clippywear <144195449+clippywear@users.noreply.github.com>
2023-09-25 22:19:25 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -304,7 +304,7 @@ function zipFiles() {
           var httpRequest = new XMLHttpRequest();
           httpRequest.open("GET", url);
           httpRequest.onload = function() {
-          	console.log("what is this.responseText", this.responseText, this)
+          	console.log("what is this.responseText", this.responseText, this, "instance of Blob?", this instanceof Blob)
             zip.file(url, this.responseText);
             resolve()
           }
